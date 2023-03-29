977f32a22d13000808a0fe89281c5e73bb14d6c4
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-03-29 22:49:43 +0200

    facade pattern

---
+++ typescript/structural/Facade/ComplexSystem.ts
@@ -0,0 +1,18 @@
+export class ComplexSystem {
+  operation1(): void {
+    console.log("starting operation1...");
+    console.log("Some logic...");
+    console.log("finishing operation1...");
+  }
+  operation2(): void {
+    console.log("starting operation2...");
+    console.log("Some logic...");
+    console.log("finishing operation2...");
+  }
+  operation3(): void {
+    console.log("starting operation3...");
+    console.log("Some logic...");
+    console.log("Another logic...");
+    console.log("finishing operation3...");
+  }
+}
---
+++ typescript/structural/Facade/test.ts
@@ -0,0 +1,5 @@
+import { Facade } from "./Facade";
+
+const facadeInterface = new Facade();
+
+facadeInterface.operation();
