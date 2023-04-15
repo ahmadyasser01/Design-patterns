f78ad2da1430884f154e8885ff352544ed06a062
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-04-15 23:58:16 +0200

    feat:cor

---
+++ typescript/Behavioral/ChainOfResponsibility/Request.ts
@@ -0,0 +1,11 @@
+export class Request {
+  private number_1: number;
+  private number_2: number;
+  private opertaion: string;
+
+  public constructor(number_1: number, number_2: number, opertaion: string) {
+    this.number_1 = number_1;
+    this.number_2 = number_2;
+    this.opertaion = opertaion;
+  }
+}
